b68df069d145d9201b3d026caa5bf15a6593d238
tony <newbiwtan@163.com>
2020-04-16 16:55:52 +0800

    save compress

---
+++ compress.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta
+from abc import abstractmethod
+from typing import ByteString
+
+import zlib
+
+
+class BaseCompress(metaclass=ABCMeta):
+    @abstractmethod
+    def compress(self, data: ByteString) -> ByteString:
+        if not isinstance(data, bytes):
+            raise TypeError(f"except a bytes-like object, but got a {type(data)}")
+        return b''
+
+    @abstractmethod
+    def decompress(self, data: ByteString) -> ByteString:
+        if not isinstance(data, bytes):
+            raise TypeError(f"except a bytes-like object, but got a {type(data)}")
+        return b''
+
+
+class ZlibCompress(BaseCompress):
+    def __init__(self, compress_level: int = 5):
+        self.compress_level = compress_level
+
+    def compress(self, data: ByteString) -> ByteString:
+        super().compress(data)
+        # need do more
+
+        return zlib.compress(data, level=self.compress_level)
+
+    def decompress(self, data: ByteString) -> ByteString:
+        super().decompress(data)
+        # need do more thing
+        return zlib.decompress(data)
